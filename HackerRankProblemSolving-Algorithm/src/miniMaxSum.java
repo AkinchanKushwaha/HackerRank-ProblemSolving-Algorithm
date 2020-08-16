612abf135bd4fe6b66d4d9b41ebcb33fa3788731
AkinchanKushwaha <akinchandroger@gmail.com>
2020-08-16 12:21:59 +0530

    HackerRank

--- HackerRankProblemSolving-Algorithm/src/miniMaxSum.java
+++ HackerRankProblemSolving-Algorithm/src/miniMaxSum.java
@@ -8,9 +8,6 @@ public class miniMaxSum {
     static void miniMaxSum(int[] arr) {
     	Arrays.sort(arr);
     	long sum = 0;
-    	for(int i = 0 ; i<arr.length; i++) {
-    		System.out.println(arr[i]);
-    	}
     	for(int i = 0 ; i< arr.length; i++) {
     		sum += arr[i];
     	}
